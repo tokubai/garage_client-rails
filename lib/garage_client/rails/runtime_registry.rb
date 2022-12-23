5ca5bb161853bbb3fc9ccdb86370b47e482e783e
Yoshiteru Negishi <negipo@gmail.com>
2022-12-23 18:37:39 +0900

    Use thread_mattr_accessor instead of deprecated  ActiveSupport::PerThreadRegistry

--- lib/garage_client/rails/runtime_registry.rb
+++ lib/garage_client/rails/runtime_registry.rb
@@ -2,8 +2,6 @@ require 'active_support/per_thread_registry'
 
 module GarageClient::Rails
   class RuntimeRegistry
-    extend ActiveSupport::PerThreadRegistry
-
-    attr_accessor :garage_client_runtime
+    thread_mattr_accessor :garage_client_runtime
   end
 end
